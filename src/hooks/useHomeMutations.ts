71ba115d15f000d94c15d070e906f5b16eefe530
chriskappa <chriskagkelis@yahoo.com>
2025-02-13 13:35:13 +0000

    Added alert when dublicate item is added

--- src/hooks/useHomeMutations.ts
+++ src/hooks/useHomeMutations.ts
@@ -2,6 +2,7 @@ import { deleteIdFromArray } from '@/lib/utils';
 import { addNewCake, deleteCake } from '@/services/cakeServices';
 import { Cake } from '@/types/types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 function useHomeMutations() {
     const queryClient = useQueryClient();
@@ -13,15 +14,28 @@ function useHomeMutations() {
             await queryClient.cancelQueries({ queryKey: ["cakes"] });
 
             const previousCakes = queryClient.getQueryData<Cake[]>(["cakes"]);
+            const optimisticCake = { ...newCake, _id: Date.now().toString() }
 
-            queryClient.setQueryData(["cakes"], (oldCakes: Cake[] | undefined) =>
-                oldCakes ? [...oldCakes, newCake] : [newCake]
+            queryClient.setQueryData(["cakes"], (oldCakes: Cake[] | undefined) => {
+                const updatedData = oldCakes ? [...oldCakes, optimisticCake] : [optimisticCake]
+                console.log({ updatedData })
+                return updatedData;
+            }
+
             );
 
             return { previousCakes };
         },
-        onError: (_err, _newCake, context) => {
+        onError: (error, _newCake, context) => {
             queryClient.setQueryData(["cakes"], context?.previousCakes);
+
+            if (error instanceof AxiosError && error.response?.data?.error?.message) {
+                const errorMessage = error.response.data.error.message;
+                const isDuplicate = errorMessage.toLowerCase().includes("duplicate");
+                if (isDuplicate) {
+                    alert("A cake with this title already exists. Please choose a unique name.");
+                }
+            }
         },
         onSettled: () => {
             invalidateCakes();
